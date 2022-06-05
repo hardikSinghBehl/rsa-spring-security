f7fea48690e38cbb0fb1aefdc9948718fafc216d
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-06-05 07:41:02 +0530

    initializing project

---
+++ src/main/java/com/behl/pehchan/RsaSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.behl.pehchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsaSpringSecurityApplication.class, args);
+	}
+
+}
